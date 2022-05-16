40979a9ac5e334c16e5eac7467a6532072f63d3f
lazy-sky <1cielo@naver.com>
2022-05-16 20:43:10 +0900

    feat: Set reducer - createSlice

--- ReduxToolkit/restaurant-example/src/app/store.ts
+++ ReduxToolkit/restaurant-example/src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import reservationReducer from '../features/reservationSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    reservation: reservationReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
